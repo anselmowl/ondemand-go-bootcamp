494d3589d7cc3b4ee9a02badc5b262dcd79bf994
Anselmo Heiras <anselmo.heiras@wizeline.com>
2023-04-20 12:33:57 -0700

    get root directory cleaned

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,19 +4,15 @@ import (
 	"go-bootcamp/api"
 	"go-bootcamp/app"
 	"go-bootcamp/pkg/pokemon"
-	"log"
 	"os"
 	"path/filepath"
 )
 
 func main() {
-	path, err := os.Getwd()
-	parent := filepath.Dir(path)
-	grandParent := filepath.Dir(parent)
-	log.Println(err)
-	log.Println(path)
-	log.Println(grandParent)
-	pokemonDAO := pokemon.NewPokemonDAO(grandParent + "/Pokemon.csv")
+	workingDirectory, _ := os.Getwd()
+	rootDirectory := filepath.Dir(filepath.Dir(workingDirectory))
+
+	pokemonDAO := pokemon.NewPokemonDAO(rootDirectory + "/Pokemon.csv")
 
 	pokemonService := pokemon.NewPokemonService(pokemonDAO)
 
